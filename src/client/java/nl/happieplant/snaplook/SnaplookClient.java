1998110c73443da90cd22beccbd8b123999e44ec
HappiePlant <tijn.luijben@outlook.com>
2025-03-11 19:27:06 +0100

    Init repo

---
+++ src/client/java/nl/happieplant/snaplook/SnaplookClient.java
@@ -0,0 +1,10 @@
+package nl.happieplant.snaplook;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SnaplookClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
