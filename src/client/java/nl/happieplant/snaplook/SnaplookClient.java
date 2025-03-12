464b13a1b0e24a4b456d20542dec6d6a5599176f
HappiePlant <tijn.luijben@outlook.com>
2025-03-12 21:03:17 +0100

    It works!

--- src/client/java/nl/happieplant/snaplook/SnaplookClient.java
+++ src/client/java/nl/happieplant/snaplook/SnaplookClient.java
@@ -6,31 +6,41 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.option.Perspective;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
 public class SnaplookClient implements ClientModInitializer {
-    private static KeyBinding keyBinding;
+    private static KeyBinding holdBackKey;
+    private static KeyBinding holdFrontKey;
+    private boolean holdingBack;
+    private boolean holdingFront;
 
     @Override
     public void onInitializeClient() {
         // This entrypoint is suitable for setting up client-specific logic, such as rendering.
-        keyBinding = KeyBindingHelper.registerKeyBinding(
+        holdBackKey = KeyBindingHelper.registerKeyBinding(
                 new KeyBinding(
                         "nl.happieplant.snaplook.hold_back",
                         InputUtil.Type.KEYSYM,
-                        GLFW.GLFW_KEY_ESCAPE,
+                        GLFW.GLFW_KEY_R,
+                        "nl.happieplant.snaplook.keybinds"
+                ));
+        holdFrontKey = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding(
+                        "nl.happieplant.snaplook.hold_front",
+                        InputUtil.Type.KEYSYM,
+                        GLFW.GLFW_KEY_V,
                         "nl.happieplant.snaplook.keybinds"
                 ));
 
+
         ClientTickEvents.END_CLIENT_TICK.register(c -> {
-            while (keyBinding.wasPressed()) {
-                c.player.sendMessage(Text.literal("Key 1 was pressed!"), false);
-                if (c.options.getPerspective() == Perspective.THIRD_PERSON_BACK) {
-                    c.options.setPerspective(Perspective.FIRST_PERSON);
-                } else {
-                    c.options.setPerspective(Perspective.THIRD_PERSON_BACK);
-                }
+            if (!holdingBack && holdBackKey.isPressed()) {
+                c.options.setPerspective(Perspective.THIRD_PERSON_BACK);
+                holdingBack = true;
+            }
+            if (holdingBack && !holdBackKey.isPressed()) {
+                c.options.setPerspective(Perspective.FIRST_PERSON);
+                holdingBack = false;
             }
         });
     }
