8f43655aa6d9fe35d5788cd62d7d2a8a4aceff3d
HappiePlant <tijn.luijben@outlook.com>
2025-03-12 21:22:22 +0100

    Try to clean up code

--- src/client/java/nl/happieplant/snaplook/SnaplookClient.java
+++ src/client/java/nl/happieplant/snaplook/SnaplookClient.java
@@ -3,6 +3,7 @@ package nl.happieplant.snaplook;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.option.Perspective;
 import net.minecraft.client.util.InputUtil;
@@ -14,34 +15,36 @@ public class SnaplookClient implements ClientModInitializer {
     private boolean holdingBack;
     private boolean holdingFront;
 
+    private static KeyBinding registerKeybind(String id) {
+        return KeyBindingHelper.registerKeyBinding(
+                new KeyBinding(
+                        "nl.happieplant.snaplook." + id,
+                        InputUtil.Type.KEYSYM,
+                        GLFW.GLFW_KEY_UNKNOWN,
+                        "nl.happieplant.snaplook.keybinds"
+                ));
+    }
+
+    private static boolean holdPerspective(MinecraftClient c, boolean holdingKey, KeyBinding key, Perspective perspective) {
+        if (!holdingKey && key.isPressed()) {
+            c.options.setPerspective(perspective);
+            return true;
+        }
+        if (holdingKey && !key.isPressed()) {
+            c.options.setPerspective(Perspective.FIRST_PERSON);
+            return false;
+        }
+        return false;
+    }
+
     @Override
     public void onInitializeClient() {
-        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
-        holdBackKey = KeyBindingHelper.registerKeyBinding(
-                new KeyBinding(
-                        "nl.happieplant.snaplook.hold_back",
-                        InputUtil.Type.KEYSYM,
-                        GLFW.GLFW_KEY_R,
-                        "nl.happieplant.snaplook.keybinds"
-                ));
-        holdFrontKey = KeyBindingHelper.registerKeyBinding(
-                new KeyBinding(
-                        "nl.happieplant.snaplook.hold_front",
-                        InputUtil.Type.KEYSYM,
-                        GLFW.GLFW_KEY_V,
-                        "nl.happieplant.snaplook.keybinds"
-                ));
-
+        holdBackKey = registerKeybind("hold_back");
+        holdFrontKey = registerKeybind("hold_front");
 
         ClientTickEvents.END_CLIENT_TICK.register(c -> {
-            if (!holdingBack && holdBackKey.isPressed()) {
-                c.options.setPerspective(Perspective.THIRD_PERSON_BACK);
-                holdingBack = true;
-            }
-            if (holdingBack && !holdBackKey.isPressed()) {
-                c.options.setPerspective(Perspective.FIRST_PERSON);
-                holdingBack = false;
-            }
+           holdingBack = holdPerspective(c, holdingBack, holdBackKey, Perspective.THIRD_PERSON_BACK);
+           holdingFront = holdPerspective(c, holdingFront, holdFrontKey, Perspective.THIRD_PERSON_FRONT);
         });
     }
 }
